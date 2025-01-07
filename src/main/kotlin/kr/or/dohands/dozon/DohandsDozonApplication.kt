9b7c2c0e32e615efedddeab030c9cc953eac54e8
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-01-07 22:42:34 +0900

    init: 프로젝트 생성

---
+++ src/main/kotlin/kr/or/dohands/dozon/DohandsDozonApplication.kt
@@ -0,0 +1,11 @@
+package kr.or.dohands.dozon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DozonApplication
+
+fun main(args: Array<String>) {
+    runApplication<DozonApplication>(*args)
+}
