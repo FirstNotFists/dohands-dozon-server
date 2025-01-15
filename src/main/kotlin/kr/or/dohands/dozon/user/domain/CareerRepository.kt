73345d1f3d86bfcc02154ce55af6024286c8e9db
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-01-15 23:31:28 +0900

    fix: data jpa native query 메소드 구성

--- src/main/kotlin/kr/or/dohands/dozon/user/domain/CareerRepository.kt
+++ src/main/kotlin/kr/or/dohands/dozon/user/domain/CareerRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 
 interface CareerRepository:JpaRepository<Career, Long>, JpaSpecificationExecutor<Career> {
+
+    fun findByName(name: String): Career
 }
--- src/main/kotlin/kr/or/dohands/dozon/user/domain/LevelExpTypeRepository.kt
+++ src/main/kotlin/kr/or/dohands/dozon/user/domain/LevelExpTypeRepository.kt
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.Query
 
 interface LevelExpTypeRepository: JpaRepository<LevelExpType, Long>, JpaSpecificationExecutor<LevelExpType> {
 
-    @Query("SELECT * FROM dohands.`level-exp-type` where  type = :type and exp > :exp order by exp;", nativeQuery = true)
+    @Query("SELECT * FROM dohands.`level-exp-type` where  type = :type and exp < :exp order by exp;", nativeQuery = true)
     fun findNextLevel(type: String, exp: Long) : List<LevelExpType>
+
+    @Query("SELECT * FROM dohands.`level-exp-type` where  type = :type and exp <= :exp order by exp desc;", nativeQuery = true)
+    fun findNextLevelNowCheck(type: String, exp: Long) : List<LevelExpType>
+//    SELECT * FROM dohands.`level-exp-type` where type = "F" and exp <= 13600 order by exp desc;
+
+    fun findByLevel(level: String): LevelExpType
 
 }
