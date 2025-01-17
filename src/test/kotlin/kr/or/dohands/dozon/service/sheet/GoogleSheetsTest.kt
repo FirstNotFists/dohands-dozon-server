fe394aa490d770b3fa63e6b050d04b4f36e4bfef
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-01-17 23:04:22 +0900

    feat: 인사평가 셀 위치를 인식하는 테스트 케이스 추가

--- src/test/kotlin/kr/or/dohands/dozon/service/sheet/GoogleSheetsTest.kt
+++ src/test/kotlin/kr/or/dohands/dozon/service/sheet/GoogleSheetsTest.kt
@@ -3,7 +3,7 @@ package kr.or.dohands.dozon.service.sheet
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.model.ValueRange
 import com.google.auth.oauth2.GoogleCredentials
-import kr.or.dohands.dozon.sheet.service.DriveService
+//import kr.or.dohands.dozon.sheet.service.DriveService
 import kr.or.dohands.dozon.sheet.service.GoogleSheetsService
 import kr.or.dohands.dozon.core.configuration.googlesheet.GoogleSheetsConfiguration
 import org.assertj.core.api.Assertions
@@ -11,10 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.Test
 
-@SpringBootTest(classes = [GoogleSheetsConfiguration::class, DriveService::class])
+@SpringBootTest(classes = [GoogleSheetsConfiguration::class])
 class GoogleSheetsTest @Autowired constructor(
     private val googleSheetsService: GoogleSheetsService,
-    private val driveService: DriveService,
 ) {
 
     @Test
@@ -24,24 +23,6 @@ class GoogleSheetsTest @Autowired constructor(
         Assertions.assertThat(result).isNotNull()
         Assertions.assertThat(result.spreadsheets()).isNotNull()
     }
-
-    @Test
-    fun `드라이브 연동을 시도한다`() {
-
-        var result = driveService.getDriveService()
-
-        Assertions.assertThat(result).isNotNull()
-    }
-
-    @Test
-    fun `드라이브에서 엑셀파일을 탐색한다`() {
-
-        var result = driveService.getDriveService()
-        println(result.files().get("서비스 기획 상세"))
-
-        Assertions.assertThat(result).isNotNull()
-    }
-
 
     @Test
     fun `credential이 정상인지 확인한다`() {
@@ -67,12 +48,14 @@ class GoogleSheetsTest @Autowired constructor(
         println(value.getValues())
     }
 
+    @Test
+    fun `셀 위치를 인식한다`() {
+        val value = googleSheetsService.getValue("인사평가", "H10:K34")
+        println(value)
+    }
+
 }
-
-
 
     // 리더부여 퀘스트 생성 기능
     // 리더부여 퀘스트 달성여부 확인
     // MAX/MEDIUM 입력시 계산
-
-
