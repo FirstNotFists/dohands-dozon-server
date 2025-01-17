4a145f17b55f5082a05236eb56ed86ac9b95c24d
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-01-17 22:58:16 +0900

    fix: api 쿼리 추가

--- src/main/kotlin/kr/or/dohands/dozon/exp/domain/ExpHistoryRepository.kt
+++ src/main/kotlin/kr/or/dohands/dozon/exp/domain/ExpHistoryRepository.kt
@@ -26,6 +26,20 @@ interface ExpHistoryRepository: JpaRepository<ExpHistory, Long> , JpaSpecificati
 
     fun findByQuestIdAndQuestType(questId: Long, questType: String) : List<ExpHistory>
 
+    @Query("select * from `exp-history` where number = :number order by updateDate limit 6", nativeQuery = true)
+    fun findByNumberOrderByUpdateDate(number: Long) : List<ExpHistory>
+
+    @Query("select * from `exp-history` where number = :number and questType = :questType order by updateDate limit 6", nativeQuery = true)
+    fun findByQuestTypeAndNumberOrderByUpdateDate(number: Long, questType: String) : List<ExpHistory>
+
+    @Query("select count(*) from `exp-history` a where questType = :questType and number = :number limit 1", nativeQuery = true)
+    fun findCountByQuestTypeAndNumberHighExp(questType: String, number: Long) : Long
+
+    @Query("SELECT a.exp, b.content, b.questName, a.updateDate, a.grade FROM dohands.`exp-history` a INNER JOIN dohands.`leader-quests` b ON a.number = b.number_number WHERE a.questType = :questType AND a.number = :number ORDER BY a.updateDate limit 6;", nativeQuery = true)
+    fun findExpHistoryWithLeaderQuests(questType: String, number: Long) : List<Array<Any>>
+
+    @Query("SELECT a.exp, b.content, b.projectName, a.updateDate, a.grade FROM dohands.`exp-history` a INNER JOIN dohands.`sword-project` b ON a.number = b.number_number WHERE a.questType = :questType AND a.number = :number ORDER BY a.updateDate limit 6;", nativeQuery = true)
+    fun findExpHistoryWithSwordProjects(questType: String, number: Long) : List<Array<Any>>
 
 
 }
