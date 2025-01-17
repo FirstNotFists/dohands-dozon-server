3c393dfb8ec5b24302330e619074cb51a405c97a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-01-17 23:03:17 +0900

    delete : 사용하지 않는 클래스 삭제

--- src/main/kotlin/kr/or/dohands/dozon/DohandsDozonApplication.kt
+++ src/main/kotlin/kr/or/dohands/dozon/DohandsDozonApplication.kt
@@ -2,10 +2,11 @@ package kr.or.dohands.dozon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class DozonApplication
-
 fun main(args: Array<String>) {
     runApplication<DozonApplication>(*args)
 }
